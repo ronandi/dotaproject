8ef1d53b5e0d711fcdc87e355314ffc18ea30b5d
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-11-29 01:19:56 -0500

    add hero role mapping

--- classifiers/base.rb
+++ classifiers/base.rb
@@ -1,6 +1,8 @@
 #Data/Config
 HERO_LIST = []
 HERO_FILE = '../data/Heroes.txt'
+HERO_ROLE_FILE = '../data/HeroesWithRoles.txt'
+ROLE_FILE = '../data/Roles.txt'
 
 #Array format
 WINS = 0
@@ -8,12 +10,47 @@ LOSSES = 1
 
 class BaseDotaClassifier
   @hero_list = []
+  @role_list = []
+  @hero_role_mapping = {}
   File.open(HERO_FILE).each do |hero|
     @hero_list << hero.chomp
+  end
+  File.open(ROLE_FILE).each do |role|
+    @role_list << role.chomp
+  end
+  File.open(HERO_ROLE_FILE).each do |hero_role|
+    hero_role = hero_role.chomp.split(',')
+    @hero_role_mapping[hero_role.first] = hero_role[1..-1]
+  end
+
+  def role_counts
+    role_count_hash = {}
+    BaseDotaClassifier.role_list.each do |role|
+      role_count_hash[role] = 0
+    end
+    return role_count_hash
+  end
+
+  def count_roles(heroes)
+    count = lambda do |team, counter|
+      team.each do |hero|
+        BaseDotaClassifier.hero_mapping[hero].each do |role|
+          counter[role] += 1
+        end
+      end
+      return counter
+    end
+    return count.call(heroes[0..4], role_counts), count.call(heroes[5..9], role_counts)
   end
 
   def self.hero_list
     @hero_list
+  end
+  def self.role_list
+    @role_list
+  end
+  def self.hero_mapping
+    @hero_role_mapping
   end
 
   #Initialization
@@ -28,6 +65,7 @@ class BaseDotaClassifier
   end
 
   def train(heroes, category)
+    radiant_roles, dire_roles = count_roles(heroes)
     if (category == 'Radiant')
       @radiant_wins += 1
       heroes[0..4].each do |hero|

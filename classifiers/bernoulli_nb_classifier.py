03432039e15e18b5f07ec14e9b3ad69a9529ab87
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-12-01 02:27:24 -0500

    fix syntax

--- classifiers/bernoulli_nb_classifier.py
+++ classifiers/bernoulli_nb_classifier.py
@@ -2,13 +2,13 @@ import numpy as np
 from sklearn.naive_bayes import BernoulliNB
 from sklearn import cross_validation
 
-X = np.loadtxt('BigVectorPlayerData.txt', dtype=np.dtype(int), delimiter=',', usecols=range(180))
-Y = np.loadtxt('BigVectorPlayerData.txt', dtype=np.dtype(int), delimiter=',', usecols=[180])
+X = np.loadtxt('../data/BigVectorPlayerData.txt', dtype=np.dtype(int), delimiter=',', usecols=range(180))
+Y = np.loadtxt('../data/BigVectorPlayerData.txt', dtype=np.dtype(int), delimiter=',', usecols=[180])
 
 classifier = BernoulliNB()
-scores = cross_validation.cross_val_score(classiifer, X, Y, cv=10)
+scores = cross_validation.cross_val_score(classifier, X, Y, cv=10)
 
-print "Accuracy: %f", scores.mean()
+print "Accuracy: %f" % scores.mean()
 
 
 
